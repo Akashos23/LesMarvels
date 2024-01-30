e1a736c46edc015d40049841b1da6921345661cd
Akashos23 <65364908+Akashos23@users.noreply.github.com>
2024-01-30 20:12:13 +0100

    Étape 3 – Tu veux ma photo ?

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-
-
+import fetch from 'node-fetch';
+import {createHash} from "node:crypto";
 /**
  * Récupère les données de l'endpoint en utilisant les identifiants
  * particuliers developer.marvels.com
@@ -7,7 +7,36 @@
  * @return {Promise<json>}
  */
 export const getData = async (url) => {
-    // A Compléter
+    const url1 = new URL(url);
+    const date = new Date();
+    const ts = date.toISOString();
+    const publicKey = "de2fca9a1c8f20f60536992bf0ded52a";
+    const privateKey = "c2233cdd3bf4e1006f23b4849767a357f7fe3b09";
+    const params = {ts : ts, apikey : publicKey, hash : await getHash(publicKey,privateKey,ts)}
+    url1.search = new URLSearchParams(params).toString();
+    const response = await fetch(url1);
+    const textResponse = await response.text();
+    const textResponseJSON = await JSON.parse(textResponse);
+    return new Promise((resolve,reject) => {
+        resolve(getDataThumbnail(textResponseJSON))
+    });
+}
+
+
+
+export const getDataThumbnail = async (resultat) => {
+   const tab = [];
+   for(let i = 0; i < resultat.data.results.length; i++){
+       if(!resultat.data.results[i].thumbnail.path.includes("image_not_available")) {
+           let tabJSON = {
+               name : resultat.data.results[i].name,
+               description : resultat.data.results[i].description,
+               imageURL : resultat.data.results[i].thumbnail.path
+           }
+           tab[i] = tabJSON;
+       }
+   }
+   return tab;
 }
 
 /**
@@ -19,5 +48,5 @@ export const getData = async (url) => {
  * @return {Promise<ArrayBuffer>} en hexadecimal
  */
 export const getHash = async (publicKey, privateKey, timestamp) => {
-    // A compléter
+    return createHash("MD5").update(timestamp + privateKey + publicKey).digest('hex')
 }
--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log("Je suis Marvel")
+import Fastify from 'fastify'
+import {getData, getDataThumbnail} from "./api.js";
+const app = Fastify()
+app.get('/getData', (req, res) => {
+        const resultat = getData("http://gateway.marvel.com/v1/public/characters");
+        resultat.then((retour) => {
+                res.send(retour);
+        }
+        )
+})
+
+
+
+
+
+app.listen({ port: 3000 })
