86780e36b0fdd0856430ea290855eaa8dc1e49d9
Akashos23 <65364908+Akashos23@users.noreply.github.com>
2024-01-31 13:17:57 +0100

    Étape 4 – On s’accroche au guidon

--- src/server.js
+++ src/server.js
@@ -1,10 +1,28 @@
 import Fastify from 'fastify'
+import handlebars from "handlebars";
+import {fastifyView} from "@fastify/view";
 import {getData, getDataThumbnail} from "./api.js";
 const app = Fastify()
-app.get('/getData', (req, res) => {
+
+app.register(fastifyView, {
+        engine: {
+                handlebars: handlebars,
+        },
+        includeViewExtension: true,
+        options: {
+                partials: {
+                        header: '../../templates/header.hbs',
+                        footer: '../../templates/footer.hbs'
+                }
+        },
+        templates : 'views'
+});
+
+
+app.get('/', (req, res) => {
         const resultat = getData("http://gateway.marvel.com/v1/public/characters");
         resultat.then((retour) => {
-                res.send(retour);
+                return res.view("../../templates/index.hbs", {retour : retour});
         }
         )
 })
